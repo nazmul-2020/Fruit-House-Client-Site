592217dca5d27b64e6c74dbb53c02babdbc740f0
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-08-25 15:35:26 +0600

    added routes

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ManageItems from '../ManageItems/ManageItems';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>home</h1>
+            <ManageItems></ManageItems>
+        </div>
+    );
+};
+
+export default Home;
